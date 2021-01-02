e43c45ed497b6961b5efd0d2e4dc7eae8160724f
terihaj <mihael.terihaj@fer.hr>
2021-01-02 22:06:19 +0100

    register services

--- microservices/aggregator-microservice/src/main/java/hr/fer/tel/rassus/aggregatorMicroservice/AggregatorMicroserviceApplication.java
+++ microservices/aggregator-microservice/src/main/java/hr/fer/tel/rassus/aggregatorMicroservice/AggregatorMicroserviceApplication.java
@@ -2,8 +2,10 @@ package hr.fer.tel.rassus.aggregatorMicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class AggregatorMicroserviceApplication {
 
 	public static void main(String[] args) {
--- microservices/temperature-microservice/src/main/java/hr/fer/tel/rassus/temperaturemicroservice/TemperatureMicroserviceApplication.java
+++ microservices/temperature-microservice/src/main/java/hr/fer/tel/rassus/temperaturemicroservice/TemperatureMicroserviceApplication.java
@@ -8,11 +8,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import java.io.FileReader;
 
 
 @SpringBootApplication
+@EnableEurekaClient
 public class TemperatureMicroserviceApplication implements CommandLineRunner {
 
 	@Autowired
