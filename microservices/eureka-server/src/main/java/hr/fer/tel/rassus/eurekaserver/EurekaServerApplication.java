4b2f1197e6eedf988e5102cf2fe8f1c8e94fe4f1
terihaj <mihael.terihaj@fer.hr>
2021-01-02 20:52:04 +0100

    init eureka-server service

---
+++ microservices/eureka-server/src/main/java/hr/fer/tel/rassus/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,13 @@
+package hr.fer.tel.rassus.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
