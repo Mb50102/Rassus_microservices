006d38989ca869f34e563fa044b3808073e00f90
st-desktop <sebastian.taras12@gmail.com>
2021-01-04 20:33:18 +0100

    restructure

---
+++ microservices/config-server-microservice/src/test/java/hr/fer/tel/rassus/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.fer.tel.rassus.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
